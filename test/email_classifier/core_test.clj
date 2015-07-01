f54500db0c34ea8870c525cb67ec9a382dab9238
Vinay Eswara <veswara@vmware.com>
2015-07-01 21:24:29 +0530

    initial commit

---
+++ test/email_classifier/core_test.clj
@@ -0,0 +1,7 @@
+(ns email-classifier.core-test
+  (:require [clojure.test :refer :all]
+            [email-classifier.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
